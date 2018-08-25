695919f9ef580dec51659291d4f527094e19aafd
Robin Markwitz <21968594@student.uwa.edu.au>
2018-08-25 18:43:31 +0800

    Update to db script

--- letter_extraction/modelAddScripts.py
+++ letter_extraction/modelAddScripts.py
@@ -2,6 +2,7 @@ from django.utils import timezone
 from db.models import *
 
 l = Location(place_name='Perth', date_modified=timezone.now(), date_added=timezone.now())
+l.save()
 p = Person(first_name='Test', last_name='Writer', full_name='Test Writer', date_added=timezone.now(), date_modified=timezone.now())
 pl = PersonLocation(location=l, person=p)
 d = Document(archive_number='test', date_written=timezone.now(), receiver=pl, sender=pl, document_type='diary', 
