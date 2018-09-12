1c29dba7b81ecfe9c81f1f5c1e5a4e6adfb1e605
Robin Markwitz <21968594@student.uwa.edu.au>
2018-09-12 12:43:08 +0800

    test

--- letter_extraction/db/views.py
+++ letter_extraction/db/views.py
@@ -41,12 +41,7 @@ def search_result(request):
     search_type = str(request.GET['searchtype'])
     query_value = str(request.GET['query'])
     document_list = query_service.analyze_query_request(search_type, query_value)
-    categories = {}
-    metadata = {}
-    storeFunction.string_split(document_list, categories, metadata)
-    print(categories)
-    print(metadata)
-    context = {'document_list': categories, "data": metadata}
+    context = {'document_list': document_list}
     return render(request, 'db/result.html', context)
 
 
