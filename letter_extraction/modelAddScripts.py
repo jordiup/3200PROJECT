0bef74a336a557c523286b99c37e5010b5d82676
Robin Markwitz <21968594@student.uwa.edu.au>
2018-09-23 09:16:25 +0800

    edited model add script to reflect changes in models

--- letter_extraction/modelAddScripts.py
+++ letter_extraction/modelAddScripts.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 from db.models import *
+from django.contrib.auth.models import User
 
 l = Location(place_name='Perth', date_modified=timezone.now(), date_added=timezone.now())
 l.save()
@@ -7,6 +8,7 @@ p = Person(first_name='Test', last_name='Writer', full_name='Test Writer', date_
 p.save()
 pl = PersonLocation(location=l, person=p)
 pl.save()
+u = User.objects.first()
 d = Document(archive_number='test', date_written=timezone.now(), receiver=pl, sender=pl, document_type='diary', 
-    language='java', date_added=timezone.now(), date_modified=timezone.now())
+    language='java', date_added=timezone.now(), date_modified=timezone.now(), uploaded_by=u)
 d.save()
