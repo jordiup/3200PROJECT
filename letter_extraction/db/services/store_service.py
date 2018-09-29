35682da0bc8616db44ea1da2465f9432a9bd7279
nicholas-yong <nickholas.yong@gmail.com>
2018-09-29 10:32:04 +0800

    Storing documents in database fix with Adi's new models(txt files)

--- letter_extraction/db/services/store_service.py
+++ letter_extraction/db/services/store_service.py
@@ -10,39 +10,40 @@ from db.models import *
 def addToModel(input, user):
     if input == {}:
         return
-    holder_of_categories = input[0]
-    archival_number = holder_of_categories[0][1]
-    language_written = holder_of_categories[5][1]
-    letter_writtenDate = holder_of_categories[2][1]
-    sender_info = re.findall(r'[^,;]+', holder_of_categories[3][1])
-    receiver_info = re.findall(r'[^,;]+', holder_of_categories[4][1])
-    spliced_language = language_written.split(',')[1].strip()
-    receiver_first_name = receiver_info[0].strip()
-    receiver_last_name = ''
-    sender_first_name = sender_info[0].strip()
-    sender_last_name = ''
-    receiver_full_name = ''.join((receiver_first_name, receiver_last_name))
-    sender_full_name= ''.join((sender_first_name, sender_last_name))
-    receiver = Person(first_name= receiver_first_name, last_name= receiver_last_name, full_name=receiver_full_name, date_added=timezone.now(), date_modified=timezone.now())
-    sender = Person(first_name=sender_first_name , last_name=sender_last_name, full_name=sender_full_name, date_added=timezone.now(), date_modified=timezone.now())
-    location_receiver = Location(place_name=receiver_info[1].strip(), date_added=timezone.now(), date_modified=timezone.now())
-    location_sender = Location(place_name=sender_info[1].strip(), date_modified=timezone.now(), date_added=timezone.now())
+    for item in range(1, len(input)):
+        holder_of_categories = input[item]
+        archival_number = holder_of_categories[0][1]
+        letter_writtenDate = holder_of_categories[2][1]
+        receiver_first_name = holder_of_categories[3][1]
+        receiver_location = holder_of_categories[4][1]
+        sender_first_name = holder_of_categories[5][1]
+        sender_location = holder_of_categories[6][1]    #Probably not the best way to do this
+        language_written = holder_of_categories[7][1]
+        spliced_language = language_written.split(',')[1].strip()
+        receiver_last_name = ''
+        sender_last_name = ''
+        receiver_full_name = ''.join((receiver_first_name, receiver_last_name))
+        sender_full_name= ''.join((sender_first_name, sender_last_name))
+        receiver = Person(first_name= receiver_first_name, last_name= receiver_last_name, full_name=receiver_full_name, date_added=timezone.now(), date_modified=timezone.now())
+        sender = Person(first_name=sender_first_name , last_name=sender_last_name, full_name=sender_full_name, date_added=timezone.now(), date_modified=timezone.now())
+        location_receiver = Location(place_name= receiver_location, date_added=timezone.now(), date_modified=timezone.now())
+        location_sender = Location(place_name=sender_location, date_modified=timezone.now(), date_added=timezone.now())
 
-    receiver.save()
-    sender.save()
-    location_receiver.save()
-    location_sender.save()
+        receiver.save()
+        sender.save()
+        location_receiver.save()
+        location_sender.save()
 
-    person_location_receiver = PersonLocation(location=location_receiver, person=receiver)
-    person_location_sender = PersonLocation(location=location_sender, person=sender)
+        person_location_receiver = PersonLocation(location=location_receiver, person=receiver)
+        person_location_sender = PersonLocation(location=location_sender, person=sender)
 
-    person_location_receiver.save()
-    person_location_sender.save()
+        person_location_receiver.save()
+        person_location_sender.save()
 
 
-    documentInstance = Document(archive_number=archival_number, date_written= letter_writtenDate, receiver=person_location_receiver, sender=person_location_sender, document_type='diary',
-        language= spliced_language , date_added=timezone.now(), date_modified=timezone.now(), uploaded_by=user)
-    documentInstance.save()
+        documentInstance = Document(archive_number=archival_number, date_written= letter_writtenDate, receiver=person_location_receiver, sender=person_location_sender, document_type='diary',
+            language= spliced_language , date_added=timezone.now(), date_modified=timezone.now(), uploaded_by=user)
+        documentInstance.save()
 
 def addToModel_xlsx(input, user):
     if input == {}:
