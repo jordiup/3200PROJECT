2e324bd33865caadeaba57a0810cf079d634d761
Robin Markwitz <21968594@student.uwa.edu.au>
2018-10-06 11:14:55 +0800

    add confirmation to add to db

--- letter_extraction/db/static/db/logoutScript.js
+++ letter_extraction/db/static/db/logoutScript.js
@@ -1,13 +1,29 @@
-function logout(){
+function logout() {
     var exitting = confirm("Are you sure you want to logout?");
-    if(exitting){
+    if (exitting) {
         var url = location.href;
         var base = url.split('db')[0];
         url = base + "db/logout/";
         location.href = url;
         return true;
     }
-    else{
+    else {
         return false;
+    }
+}
+
+function mydiscard() {
+    var url = location.href;
+    var base = url.split('db')[0];
+    url = base + "db/upload";
+    location.href = url;
+}
+
+function addToDb() {
+    var button = document.getElementById("commit-button");
+    var add = confirm("Are you sure you want to add this file?");
+    if(!add) {
+        var form = document.getElementById("input_field");
+        form.action = "/db/upload/";
     }
 }
