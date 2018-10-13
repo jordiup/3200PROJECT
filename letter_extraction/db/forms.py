131c759a1c976bebf5ce463d2fd1094bdb183bc6
Robin Markwitz <21968594@student.uwa.edu.au>
2018-10-13 16:25:50 +0800

    more modifications to edit page and minor bug fixes

--- letter_extraction/db/forms.py
+++ letter_extraction/db/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import *
+from django.db.models import Q
 
 class documentForm(forms.ModelForm):
     class Meta:
@@ -18,13 +19,60 @@ class personLocationForm(forms.ModelForm):
         model = PersonLocation
         fields = '__all__'
 
+    def save_form(self):
+        if self.is_valid():
+            person_location = self.save()
+            person_location.save()
+            return True
+
 
 class personForm(forms.ModelForm):
     class Meta:
         model = Person
         fields = ['first_name', 'last_name', 'full_name']
 
+    def save_form(self):
+        if self.is_valid():
+            person = self.save()
+            person.save()
+            return True
+
 class locationForm(forms.ModelForm):
     class Meta:
         model = Location
         fields = ['place_name']
+
+    def save_form(self):
+        if self.is_valid():
+            location = self.save()
+            location.save()
+            return True
+
+
+def get_forms_by_document(request, id):
+    forms = []
+    document = Document.objects.filter(pk=id).first()
+    sender_pl = PersonLocation.objects.filter(pk=document.sender.pk).first()
+    receiver_pl = PersonLocation.objects.filter(pk=document.sender.pk).first()
+    sender_person = Person.objects.filter(pk=sender_pl.person.pk).first()
+    sender_location = Location.objects.filter(pk=sender_pl.location.pk).first()
+    receiver_person = Person.objects.filter(pk=receiver_pl.person.pk).first()
+    receiver_location = Location.objects.filter(pk=receiver_pl.location.pk).first()
+
+    document_form_post = documentForm(request.POST, instance=document)
+    forms.append(document_form_post)
+    document_form = documentForm(instance=document)
+    forms.append(document_form)
+    sender_form = personForm(instance=sender_person)
+    forms.append(sender_form)
+    receiver_form = personForm(instance=receiver_person)
+    forms.append(receiver_form)
+    sender_location_form = locationForm(instance=sender_location)
+    forms.append(sender_location_form)
+    receiver_location_form = locationForm(instance=receiver_location)
+    forms.append(receiver_location_form)
+
+    return forms
+
+
+
--- letter_extraction/db/views.py
+++ letter_extraction/db/views.py
@@ -65,7 +65,7 @@ def search_result(request):
     search_type = str(request.GET['searchtype'])
     query_value = str(request.GET['query'])
     values = query_service.analyze_query_request(search_type, query_value, False)
-    header = ['Archive Number', 'Date Written', 'Document Type', 'Language', 'Place Written', 'Sender Name', 'Receiver Name', 'Chnage']
+    header = ['Archive Number', 'Date Written', 'Document Type', 'Language', 'Place Written', 'Sender Name', 'Receiver Name', 'Edit']
     #primary_keys = query_service.get_pks(values)
     context = {'header': header, 'values': values}
     return render(request, 'db/result.html', context)
@@ -96,26 +96,24 @@ def upload(request):
 def edit(request, items):
     if not request.user.has_perm('db.can_edit'):
         return render(request, 'db/index.html', {"message":"You do not have the permissions to perform this task!"})
-    global previous_url
-    if request.method != "POST":
-        previous_url = request.META.get('HTTP_REFERER')
-    document_object = Document.objects.filter(pk=items).first()
-    print(document_object.sender)
-    personLocation_object = PersonLocation.objects.filter(pk=document_object.sender.pk).first()
-    person_Object = Person.objects.filter(pk=personLocation_object.person.pk).first()
-    location_Object = Location.objects.filter(pk=personLocation_object.location.pk).first()
+    forms = get_forms_by_document(request, items)
     if request.method == "POST":
-        document_test_form = documentForm(request.POST, instance=document_object)
-        if document_test_form.is_valid():
-            instance = document_test_form.save(commit=False)
+        if forms[0].is_valid():
+            instance = forms[0].save(commit=False)
             instance.save()
-            return redirect(previous_url)
+            context["update_message"] = 'Successfully updated!'
+            return render(request, 'db/edit.html', context)
         else:
-            return HttpResponse("<p>Your Modification is invalid </p")
-    document_test_form = documentForm(instance=document_object)
-    person_test_form = personForm(instance=person_Object)
-    location_test_form = locationForm(instance=location_Object)
-    return render(request, 'db/edit.html', {'document_test_form' : document_test_form, 'person_test_form' : person_test_form, 'location_test_form' : location_test_form} )
+            context["update_message"] = 'Update failed!'
+            return render(request, 'db/edit.html', context)   
+    context = {
+        'document_form' : forms[1],
+        'sender_form' : forms[2],
+        'receiver_form' : forms[3],
+        'sender_location_form' : forms[4],
+        'receiver_location_form' : forms[5],
+        }
+    return render(request, 'db/edit.html', context)
 
 
 def login_user(request):
